f7caa65abeb35c95054f7f1a1fdb64dd05d5c2fd
Cian Barrett <G00416157@atu.ie>
2024-06-03 17:44:31 +0100

    added custom durations

--- chatplayer.py
+++ chatplayer.py
@@ -4,11 +4,14 @@ import threading
 import time
 SERVER = "irc.twitch.tv"
 PORT = 6667
-PASS = "TWITCH OAUTH PASS"
+PASS = "TWITCHOAUTHPASS"
 BOT = "bot"
 CHANNEL = "#cianrr"  # Ensure the channel name is prefixed with '#'
 OWNER = "cianrr"
+
 message = ""
+duration = 0
+
 irc = socket.socket()
 irc.connect((SERVER, PORT))
 irc.send((  "PASS " + PASS + "\r\n" +
@@ -20,28 +23,32 @@ for i in list(range(4))[::-1]:
     time.sleep(0.5)
 
 def gameControl():
-    global message
+    global message, duration
     while True:
         if "forward" in message.lower():
             PressKey(W)
-            time.sleep(3)
+            time.sleep(duration)
             ReleaseKey(W)
             message = ""
+            duration = 0
         elif "left" in message.lower():
             PressKey(A)
-            time.sleep(3)
+            time.sleep(duration)
             ReleaseKey(A)
             message = ""
+            duration = 0
         elif "down" in message.lower():
             PressKey(S)
-            time.sleep(3)
+            time.sleep(duration)
             ReleaseKey(S)
             message = ""
+            duration = 0
         elif "right" in message.lower():
             PressKey(D)
-            time.sleep(3)
+            time.sleep(duration)
             ReleaseKey(D)
             message = ""
+            duration = 0
         else:
             pass
 
@@ -63,6 +70,7 @@ def twitch():
             return False
         else:
             return True
+        
     def sendMessage(irc, message):
         messageTmp = "PRIVMSG " + CHANNEL + " :" + message
         irc.send((messageTmp + "\n").encode())
@@ -73,12 +81,19 @@ def twitch():
         return user
 
     def getMessage(line):
-        global message
+        global message, duration
         try:
             message = (line.split(":", 2))[2]
+            message_parts = message.rsplit(" ", 1)
+            message = message_parts[0]
+            duration = int(message_parts[1])
+            if duration > 10:
+                duration = 3
         except:
             message = ""
-        return message
+            duration = 0
+        return message , duration
+    
     def Console(line):
         if "PRIVMSG" in line:
             return False
@@ -96,13 +111,13 @@ def twitch():
             if line == "":
                 continue
             elif "PING" in line and Console(line):
-                msgg = "PONG tmi.twitch.tv\r\n".encode() # have to return a pong to stay connected
+                msgg = "PONG tmi.twitch.tv\r\n".encode() # have to return a pong to stay connectedd
                 irc.send(msgg)
                 print(msgg)
                 continue
             else:
                 user = getUser(line)
-                message = getMessage(line)
+                message, duration = getMessage(line)
                 print(user + " : " + message)
 
 if __name__ == '__main__':
