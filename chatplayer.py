06e682278fb1f842085d7deb56f7ea2754534ef5
Cian Barrett <cianbarrett3011@gmail.com>
2024-08-19 15:52:29 +0100

    added hotkey listener to stop receiving messages

--- chatplayer.py
+++ chatplayer.py
@@ -4,6 +4,7 @@ import threading
 import pygame
 import vgamepad as vg
 from directkeys import PressKey,ReleaseKey,W,A,S,D,SPACE
+import keyboard
 
 print(r"""
 
@@ -32,12 +33,13 @@ joysticks = [pygame.joystick.Joystick(x) for x in range (pygame.joystick.get_cou
 
 for joystick in joysticks:
     joystick.init()
-    print("INPUT DEVICE DETECTED:", joystick.get_name())
+    print("INPUT DEVICE DETECTED: ", joystick.get_name(), ", VIA: ", joystick.get_power_level(), ", GUID:", joystick.get_guid())
 
 message = ""
 duration = 0
 input_condition = 0
 lock = threading.Lock()
+exit_flag = False
 
 input_condition = int(input("WHICH INPUT ARE YOU USING- 1-Controller 2-Keyboard: "))
 
@@ -54,10 +56,18 @@ for i in list(range(4))[::-1]:
     print(i+1)
     time.sleep(0.5)
 
+def hotkeyListener():
+    global exit_flag
+    while not exit_flag:
+        if keyboard.is_pressed('shift+backspace'):
+            print('Stopped all threads')
+            exit_flag = True
+        time.sleep(0.1)\
+
 def gameControl():
     global message, duration, input_condition
 
-    while True:
+    while not exit_flag:      
         with lock:
             msg = message.lower()
             dur = duration
@@ -66,56 +76,56 @@ def gameControl():
 
         if input_condition == 1:
             if "triangle" in msg:
-                print(f"Pressing forward for {dur}")
-                gamepad.press_button(button=vg.DS4_BUTTONS.DS4_BUTTON_CROSS)
+                print(f"[",joystick.get_name(),"]",f"triangle for {dur}")
+                gamepad.press_button(button=vg.DS4_BUTTONS.DS4_BUTTON_TRIANGLE)
                 gamepad.update()
                 time.sleep(dur)
                 gamepad.release_button(button=vg.DS4_BUTTONS.DS4_BUTTON_TRIANGLE)
                 gamepad.update()
             elif "square" in msg:
-                print(f"Pressing left for {dur}")
+                print(f"[",joystick.get_name(),"]",f"square for {dur}")
                 gamepad.press_button(button=vg.DS4_BUTTONS.DS4_BUTTON_SQUARE)
                 gamepad.update()
                 time.sleep(dur)
                 gamepad.release_button(button=vg.DS4_BUTTONS.DS4_BUTTON_SQUARE)
                 gamepad.update()
             elif "cross" in msg:
-                print(f"Pressing down for {dur}")
+                print(f"[",joystick.get_name(),"]",f"cross for {dur}")
                 gamepad.press_button(button=vg.DS4_BUTTONS.DS4_BUTTON_CROSS)
                 gamepad.update()
                 time.sleep(dur)
                 gamepad.release_button(button=vg.DS4_BUTTONS.DS4_BUTTON_CROSS)
                 gamepad.update()
             elif "circle" in msg:
-                print(f"Pressing right for {dur}")
+                print(f"[",joystick.get_name(),"]",f"circle for {dur}")
                 gamepad.press_button(button=vg.DS4_BUTTONS.DS4_BUTTON_CIRCLE)
                 gamepad.update()
                 time.sleep(dur)
                 gamepad.release_button(button=vg.DS4_BUTTONS.DS4_BUTTON_CIRCLE)
                 gamepad.update()
             elif "forward" in msg:
-                print(f"Pressing forward for {dur}")
+                print(f"[",joystick.get_name(),"]",f"forward for {dur}")
                 gamepad.left_joystick_float(x_value_float=0.0, y_value_float=-1.0)
                 gamepad.update()
                 time.sleep(dur)
                 gamepad.left_joystick_float(x_value_float=0.0, y_value_float=0.0)
                 gamepad.update()
             elif "left" in msg:
-                print(f"Pressing left for {dur}")
+                print(f"[",joystick.get_name(),"]",f"left for {dur}")
                 gamepad.left_joystick_float(x_value_float=-1.0, y_value_float=0.0)
                 gamepad.update()
                 time.sleep(dur)
                 gamepad.left_joystick_float(x_value_float=0.0, y_value_float=0.0)
                 gamepad.update()
             elif "right" in msg:
-                print(f"Pressing right for {dur}")
+                print(f"[",joystick.get_name(),"]",f"right for {dur}")
                 gamepad.left_joystick_float(x_value_float=1.0, y_value_float=0.0)
                 gamepad.update()
                 time.sleep(dur)
                 gamepad.left_joystick_float(x_value_float=0.0, y_value_float=0.0)
                 gamepad.update()
             elif "down" in msg:
-                print(f"Pressing down for {dur}")
+                print(f"[",joystick.get_name(),"]",f"down for {dur}")
                 gamepad.left_joystick_float(x_value_float=0.0, y_value_float=1.0)
                 gamepad.update()
                 time.sleep(dur)
@@ -148,84 +158,97 @@ def gameControl():
                 PressKey(SPACE)
                 time.sleep(dur)
                 ReleaseKey(SPACE)
+            time.sleep(0.1)
 
 def twitch():
-    def joinchat():
-        Loading = True
-        while Loading:
-            readbuffer_join = irc.recv(1024).decode()
-            for line in readbuffer_join.split("\n")[0:-1]:
-                print(line)
-                Loading = loadingComplete(line)
 
-    def loadingComplete(line):
-        if "End of /NAMES list" in line:
-            print("Bot has joined " + CHANNEL + "'s Channel!")
-            sendMessage(irc, "<CHAT PLAYER ENABLED>")
-            sendMessage(irc, "MOVEMENT FUNCTIONS: forward, left, right, down")
-            return False
-        return True
+    global exit_flag
+    while not exit_flag:
+        def joinchat():
+            Loading = True
+            while Loading:
+                readbuffer_join = irc.recv(1024).decode()
+                for line in readbuffer_join.split("\n")[0:-1]:
+                    print(line)
+                    Loading = loadingComplete(line)
+
+        def loadingComplete(line):
+            if "End of /NAMES list" in line:
+                print("Bot has joined " + CHANNEL + "'s Channel!")
+                sendMessage(irc, "<CHAT PLAYER ENABLED>")
+                sendMessage(irc, "MOVEMENT FUNCTIONS: forward, left, right, down")
+                return False
+            return True
+            
+        def sendMessage(irc, message):
+            messageTmp = "PRIVMSG " + CHANNEL + " :" + message
+            irc.send((messageTmp + "\n").encode())
+
+        def getUser(line):
+            return line.split(":", 2)[1].split("!", 1)[0] 
+
+        def getMessage(line):
+            global message, duration
+            try:
+                msg = (line.split(":", 2))[2].strip()
+                message_parts = msg.rsplit(" ", 1)
+                if len(message_parts) > 1 and message_parts[1].isdigit():
+                    msg = message_parts[0]
+                    dur = int(message_parts[1])
+                #sets default duration to 3 if none is set
+                else:
+                    dur = 3
+                if dur > 10:
+                    dur = 3
+            except:
+                msg = ""
+                dur = 3
+            with lock:
+                message = msg
+                duration = dur
+            return msg, dur
         
-    def sendMessage(irc, message):
-        messageTmp = "PRIVMSG " + CHANNEL + " :" + message
-        irc.send((messageTmp + "\n").encode())
+        #checks that message format is handled
+        def checkMessage(irc, msg):
+            user_message, dur = getMessage(msg)
+            return True
+        
+        def Console(line):
+            return not "PRIVMSG" in line
 
-    def getUser(line):
-        return line.split(":", 2)[1].split("!", 1)[0] 
+        joinchat()
 
-    def getMessage(line):
-        global message, duration
-        try:
-            msg = (line.split(":", 2))[2].strip()
-            message_parts = msg.rsplit(" ", 1)
-            if len(message_parts) > 1 and message_parts[1].isdigit():
-                msg = message_parts[0]
-                dur = int(message_parts[1])
-            #sets default duration to 3 if none is set
-            else:
-                dur = 3
-            if dur > 10:
-                dur = 3
-        except:
-            msg = ""
-            dur = 3
-        with lock:
-            message = msg
-            duration = dur
-        return msg, dur
-    
-    #checks that message format is handled
-    def checkMessage(irc, msg):
-        user_message, dur = getMessage(msg)
-        return True
-    
-    def Console(line):
-        return not "PRIVMSG" in line
-
-    joinchat()
-
-    while True:
-        try: 
-            readbuffer = irc.recv(1024).decode()
-        except:
-            readbuffer = ""
-        for line in readbuffer.split("\r\n"):
-            if line == "":
-                continue
-            elif "PING" in line and Console(line):
-                irc.send("PONG tmi.twitch.tv\r\n".encode())
-                continue
-            else:
-                user = getUser(line)
-                if checkMessage(irc, line):
-                    msg, dur = getMessage(line)
-                    print(f"Received message from {user}: {msg} for {dur} seconds")
-                    with lock:
-                        message = msg
-                        duration = dur
+        while True:
+            try: 
+                readbuffer = irc.recv(1024).decode()
+            except:
+                readbuffer = ""
+            for line in readbuffer.split("\r\n"):
+                if line == "":
+                    continue
+                elif "PING" in line and Console(line):
+                    irc.send("PONG tmi.twitch.tv\r\n".encode())
+                    continue
+                else:
+                    user = getUser(line)
+                    if checkMessage(irc, line):
+                        msg, dur = getMessage(line)
+                        with lock:
+                            message = msg
+                            duration = dur
+                
 
 if __name__ == '__main__':
     t1 = threading.Thread(target=twitch)
     t1.start()
     t2 = threading.Thread(target=gameControl)
     t2.start()
+    t3 = threading.Thread(target=hotkeyListener)
+    t3.start()
+
+    t1.join()
+    t2.join()
+    t3.join()
+
+    print('program exited.')
+
