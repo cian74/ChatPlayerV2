b18e6c87e04ef3fb7f607552bfbb26cbfaf1d073
cian74 <cianbarrett3011@gmail.com>
2024-10-17 22:41:49 +0100

    seperated twitch connection logic from game control logic. Also fixed threading issue with keyboard methods.

--- chatplayer.py
+++ chatplayer.py
@@ -1,11 +1,10 @@
-import socket
 import time
-import threading
 import pygame
 import vgamepad as vg
 from directkeys import PressKey, ReleaseKey, W, A, S, D, SPACE
-import signal
-import sys
+import twitch_connection
+import concurrent.futures
+import keyboard
 
 print(r"""
 
@@ -18,16 +17,6 @@ _________ .__            __ __________.__                              ___.     
 
  """)
 
-# Read the OAuth key from file
-with open('oauthkey.txt', 'r') as file:
-    PASS = file.readline().strip()
-
-SERVER = "irc.twitch.tv"
-PORT = 6667
-BOT = "bot"
-CHANNEL = "#cianrr"  # Ensure the channel name is prefixed with '#'
-OWNER = "cianrr"
-
 pygame.init()
 
 joysticks = [pygame.joystick.Joystick(x) for x in range(pygame.joystick.get_count())]
@@ -36,225 +25,130 @@ for joystick in joysticks:
     joystick.init()
     print("INPUT DEVICE DETECTED: ", joystick.get_name(), ", VIA: ", joystick.get_power_level(), ", GUID:", joystick.get_guid())
 
+thread_pool = concurrent.futures.ThreadPoolExecutor(max_workers=100)
 message = ""
 duration = 0
 message_counter = 0
 input_condition = 0
-lock = threading.Lock()
-exit_flag = False
+active_tasks = []
 
-def signal_handler(sig, frame):
-    global exit_flag
-    exit_flag = True
-    print('You pressed Ctrl+C!')
+t = twitch_connection.Twitch()
 
-signal.signal(signal.SIGINT, signal_handler)
 
 input_condition = int(input("WHICH INPUT ARE YOU USING- 1-Controller 2-Keyboard: "))
 
 if input_condition == 1:
     gamepad = vg.VDS4Gamepad()
 
-irc = socket.socket()
-irc.connect((SERVER, PORT))
-irc.send((  "PASS " + PASS + "\r\n" +
-            "NICK " + BOT + "\r\n" + 
-            "JOIN " + CHANNEL + "\r\n").encode())
-
 for i in list(range(4))[::-1]:
     print(i+1)
     time.sleep(0.5)
     
-def gameControl():
-    global message, duration, input_condition
+print(f"connected to channel: {t.channel}")
 
-    while not exit_flag:      
-        with lock:
-            msg = message.lower()
-            dur = duration
-            message = ""
-            duration = 0
+def gameControl(msg,dur):
+    global input_condition
 
-        if input_condition == 1:
-            if "triangle" in msg:
-                print(f"[",joystick.get_name(),"]",f"triangle for {dur}")
-                gamepad.press_button(button=vg.DS4_BUTTONS.DS4_BUTTON_TRIANGLE)
-                gamepad.update()
-                time.sleep(dur)
-                gamepad.release_button(button=vg.DS4_BUTTONS.DS4_BUTTON_TRIANGLE)
-                gamepad.update()
-            elif "square" in msg:
-                print(f"[",joystick.get_name(),"]",f"square for {dur}")
-                gamepad.press_button(button=vg.DS4_BUTTONS.DS4_BUTTON_SQUARE)
-                gamepad.update()
-                time.sleep(dur)
-                gamepad.release_button(button=vg.DS4_BUTTONS.DS4_BUTTON_SQUARE)
-                gamepad.update()
-            elif "cross" in msg:
-                print(f"[",joystick.get_name(),"]",f"cross for {dur}")
-                gamepad.press_button(button=vg.DS4_BUTTONS.DS4_BUTTON_CROSS)
-                gamepad.update()
-                time.sleep(dur)
-                gamepad.release_button(button=vg.DS4_BUTTONS.DS4_BUTTON_CROSS)
-                gamepad.update()
-            elif "circle" in msg:
-                print(f"[",joystick.get_name(),"]",f"circle for {dur}")
-                gamepad.press_button(button=vg.DS4_BUTTONS.DS4_BUTTON_CIRCLE)
-                gamepad.update()
-                time.sleep(dur)
-                gamepad.release_button(button=vg.DS4_BUTTONS.DS4_BUTTON_CIRCLE)
-                gamepad.update()
-            elif "forward" in msg:
-                print(f"[",joystick.get_name(),"]",f"forward for {dur}")
-                gamepad.left_joystick_float(x_value_float=0.0, y_value_float=-1.0)
-                gamepad.update()
-                time.sleep(dur)
-                gamepad.left_joystick_float(x_value_float=0.0, y_value_float=0.0)
-                gamepad.update()
-            elif "left" in msg:
-                print(f"[",joystick.get_name(),"]",f"left for {dur}")
-                gamepad.left_joystick_float(x_value_float=-1.0, y_value_float=0.0)
-                gamepad.update()
-                time.sleep(dur)
-                gamepad.left_joystick_float(x_value_float=0.0, y_value_float=0.0)
-                gamepad.update()
-            elif "right" in msg:
-                print(f"[",joystick.get_name(),"]",f"right for {dur}")
-                gamepad.left_joystick_float(x_value_float=1.0, y_value_float=0.0)
-                gamepad.update()
-                time.sleep(dur)
-                gamepad.left_joystick_float(x_value_float=0.0, y_value_float=0.0)
-                gamepad.update()
-            elif "down" in msg:
-                print(f"[",joystick.get_name(),"]",f"down for {dur}")
-                gamepad.left_joystick_float(x_value_float=0.0, y_value_float=1.0)
-                gamepad.update()
-                time.sleep(dur)
-                gamepad.left_joystick_float(x_value_float=0.0, y_value_float=0.0)
-                gamepad.update()
+    if input_condition == 1:
+        if "triangle" in msg:
+            print(f"[",joystick.get_name(),"]",f"triangle for {dur}")
+            gamepad.press_button(button=vg.DS4_BUTTONS.DS4_BUTTON_TRIANGLE)
+            gamepad.update()
+            time.sleep(dur)
+            gamepad.release_button(button=vg.DS4_BUTTONS.DS4_BUTTON_TRIANGLE)
+            gamepad.update()
+        elif "square" in msg:
+            print(f"[",joystick.get_name(),"]",f"square for {dur}")
+            gamepad.press_button(button=vg.DS4_BUTTONS.DS4_BUTTON_SQUARE)
+            gamepad.update()
+            time.sleep(dur)
+            gamepad.release_button(button=vg.DS4_BUTTONS.DS4_BUTTON_SQUARE)
+            gamepad.update()
+        elif "cross" in msg:
+            print(f"[",joystick.get_name(),"]",f"cross for {dur}")
+            gamepad.press_button(button=vg.DS4_BUTTONS.DS4_BUTTON_CROSS)
+            gamepad.update()
+            time.sleep(dur)
+            gamepad.release_button(button=vg.DS4_BUTTONS.DS4_BUTTON_CROSS)
+            gamepad.update()
+        elif "circle" in msg:
+            print(f"[",joystick.get_name(),"]",f"circle for {dur}")
+            gamepad.press_button(button=vg.DS4_BUTTONS.DS4_BUTTON_CIRCLE)
+            gamepad.update()
+            time.sleep(dur)
+            gamepad.release_button(button=vg.DS4_BUTTONS.DS4_BUTTON_CIRCLE)
+            gamepad.update()
+        elif "forward" in msg:
+            print(f"[",joystick.get_name(),"]",f"forward for {dur}")
+            gamepad.left_joystick_float(x_value_float=0.0, y_value_float=-1.0)
+            gamepad.update()
+            time.sleep(dur)
+            gamepad.left_joystick_float(x_value_float=0.0, y_value_float=0.0)
+            gamepad.update()
+        elif "left" in msg:
+            print(f"[",joystick.get_name(),"]",f"left for {dur}")
+            gamepad.left_joystick_float(x_value_float=-1.0, y_value_float=0.0)
+            gamepad.update()
+            time.sleep(dur)
+            gamepad.left_joystick_float(x_value_float=0.0, y_value_float=0.0)
+            gamepad.update()
+        elif "right" in msg:
+            print(f"[",joystick.get_name(),"]",f"right for {dur}")
+            gamepad.left_joystick_float(x_value_float=1.0, y_value_float=0.0)
+            gamepad.update()
+            time.sleep(dur)
+            gamepad.left_joystick_float(x_value_float=0.0, y_value_float=0.0)
+            gamepad.update()
+        elif "down" in msg:
+            print(f"[",joystick.get_name(),"]",f"down for {dur}")
+            gamepad.left_joystick_float(x_value_float=0.0, y_value_float=1.0)
+            gamepad.update()
+            time.sleep(dur)
+            gamepad.left_joystick_float(x_value_float=0.0, y_value_float=0.0)
+            gamepad.update()
 
-        elif input_condition == 2:
-            if "forward" in msg:
-                print(f"Pressing forward for {dur}")
-                PressKey(W)
-                time.sleep(dur)
-                ReleaseKey(W)
-            elif "left" in msg:
-                print(f"Pressing left for {dur}")
-                PressKey(A)
-                time.sleep(dur)
-                ReleaseKey(A)
-            elif "down" in msg:
-                print(f"Pressing down for {dur}")
-                PressKey(S)
-                time.sleep(dur)
-                ReleaseKey(S)
-            elif "right" in msg:
-                print(f"Pressing right for {dur}")
-                PressKey(D)
-                time.sleep(dur)
-                ReleaseKey(D)
-            elif "space" in msg:
-                print(f"Pressing space for {dur}")
-                PressKey(SPACE)
-                time.sleep(dur)
-                ReleaseKey(SPACE)
-            time.sleep(0.1)
+    elif input_condition == 2:
+        if "forward" in msg:
+            print(f"Pressing forward for {dur}")
+            PressKey(W)
+            time.sleep(dur)
+            ReleaseKey(W)
+        elif "left" in msg:
+            print(f"Pressing left for {dur}")
+            PressKey(A)
+            time.sleep(dur)
+            ReleaseKey(A)
+        elif "down" in msg:
+            print(f"Pressing down for {dur}")
+            PressKey(S)
+            time.sleep(dur)
+            ReleaseKey(S)
+        elif "right" in msg:
+            print(f"Pressing right for {dur}")
+            PressKey(D)
+            time.sleep(dur)
+            ReleaseKey(D)
+        elif "space" in msg:
+            print(f"Pressing space for {dur}")
+            PressKey(SPACE)
+            time.sleep(dur)
+            ReleaseKey(SPACE)
+        time.sleep(0.1)
 
-def twitch():
-    global exit_flag
-    while not exit_flag:
-        def joinchat():
-            Loading = True
-            while Loading:
-                readbuffer_join = irc.recv(1024).decode()
-                for line in readbuffer_join.split("\n")[0:-1]:
-                    print(line)
-                    Loading = loadingComplete(line)
-
-        def loadingComplete(line):
-            if "End of /NAMES list" in line:
-                print("Bot has joined " + CHANNEL + "'s Channel!")
-                sendMessage(irc, "<CHAT PLAYER ENABLED>")
-                sendMessage(irc, "MOVEMENT FUNCTIONS: forward, left, right, down")
-                return False
-            return True
-        
-        def countMessage(msg):
-            global message_counter
-            with lock:
-                if "left" in msg or "right" in msg:
-                    message_counter += 1
-                    print("counter total:", message_counter)
-                if message_counter % 15 == 0:
-                    sleepMode()
-
-        def sleepMode():
-            sendMessage(irc, "<GRACE PERIOD GRANTED>")
-            time.sleep(10)
-
-        def sendMessage(irc, message):
-            messageTmp = "PRIVMSG " + CHANNEL + " :" + message
-            irc.send((messageTmp + "\n").encode())
-
-        def getUser(line):
-            return line.split(":", 2)[1].split("!", 1)[0] 
-
-        def getMessage(line):
-            global message, duration
-            try:
-                msg = (line.split(":", 2))[2].strip()
-                message_parts = msg.rsplit(" ", 1)
-                if len(message_parts) > 1 and message_parts[1].isdigit():
-                    msg = message_parts[0]
-                    dur = int(message_parts[1])
-                else:
-                    dur = 3
-                if dur > 10:
-                    dur = 3
-            except:
-                msg = ""
-                dur = 3
-            with lock:
-                message = msg
-                duration = dur
-            return msg, dur
-        
-        def checkMessage(irc, msg):
-            user_message, dur = getMessage(msg)
-            return True
-        
-        def Console(line):
-            return not "PRIVMSG" in line
-
-        joinchat()
-
-        while not exit_flag:
-            try: 
-                readbuffer = irc.recv(1024).decode()
-            except:
-                readbuffer = ""
-            for line in readbuffer.split("\r\n"):
-                if line == "":
-                    continue
-                elif "PING" in line and Console(line):
-                    irc.send("PONG tmi.twitch.tv\r\n".encode())
-                    continue
-                else:
-                    user = getUser(line)
-                    if checkMessage(irc, line):
-                        msg, dur = getMessage(line)
-                        countMessage(msg)
-                        with lock:
-                            message = msg
-                            duration = dur
+def handle_messages():
+    while True:
+        line = t.receive_message()
+        if line:
+            message, duration = t.process_message(line)
+            if message and duration:
+                print(f"Messsage: {message}, Duration: {duration}")
+            
+                active_tasks.append(thread_pool.submit(gameControl, message, duration))
                 
-if __name__ == '__main__':
-    t1 = threading.Thread(target=twitch)
-    t1.start()
-    t2 = threading.Thread(target=gameControl)
-    t2.start()
+    
+thread_pool.submit(handle_messages)
 
-    t1.join()
-    t2.join()
-    print("Program terminated.")
+while True:
+    if keyboard.is_pressed("shift+backspace"):
+        print("Exiting...")
+        exit()
---
+++ twitch_connection.py
@@ -0,0 +1,70 @@
+import socket
+import time
+import concurrent.futures
+
+class Twitch:
+    def __init__(self, server="irc.chat.twitch.tv", port=6667, channel="#cianrr", bot="bot"):
+        self.server = server
+        self.port = port
+        self.channel = channel
+        self.bot = bot
+        self.message = ""
+        self.duration = 0
+        self.message_counter = 0
+
+        # Load OAuth key from file
+        with open('oauthkey.txt', 'r') as file:
+            self.oauth_key = file.readline().strip()
+
+        self.irc = socket.socket()
+        self.connect()
+
+        self.exucutor = concurrent.futures.ThreadPoolExecutor(max_workers=100)
+
+        self.send_message("Connected to chat!.")
+
+    def connect(self):
+        """Connect to the Twitch IRC server and join the channel."""
+        self.irc.connect((self.server, self.port))
+        self.irc.send((f"PASS {self.oauth_key}\r\nNICK {self.bot}\r\nJOIN {self.channel}\r\n").encode())
+        print(f"Connecting to {self.channel}'s channel...")
+
+
+    def send_message(self, message):
+        """Send a message to the Twitch chat."""
+        message_tmp = f"PRIVMSG {self.channel} :{message}"
+        self.irc.send((message_tmp + "\n").encode())
+
+    def receive_message(self):
+        """Receive a message from the Twitch IRC server."""
+        try:
+            readbuffer = self.irc.recv(1024).decode()
+        except socket.error:
+            return None
+
+        messages = readbuffer.split("\r\n")
+        for line in messages:
+            if line == "":
+                continue
+            if "PING" in line:
+                self.irc.send("PONG tmi.twitch.tv\r\n".encode())
+            else:
+                return line
+        return None
+
+    def process_message(self, line):
+        """Extract and return the message and duration from a Twitch chat message."""
+        try:
+            msg = (line.split(":", 2))[2].strip().lower()
+            message_parts = msg.rsplit(" ", 1)
+            if len(message_parts) > 1 and message_parts[1].isdigit():
+                msg = message_parts[0]
+                dur = int(message_parts[1])
+            else:
+                dur = 3
+            if dur > 10:
+                dur = 3
+        except IndexError:
+            msg = ""
+            dur = 3
+        return msg, dur
